86106a61c055bc3fa8983ae567caea25236858be
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2025-01-14 14:50:19 +0100

    updated environment

--- setup.sh
+++ setup.sh
@@ -3,18 +3,14 @@
 # Set environment name
 ENV_NAME="XAI"
 
-echo "Creating the Conda environment..."
+echo "Creating the Conda environment using environment.yml..."
 
-# Create the Conda environment with a specific Python version (use the desired version)
-conda create -n $ENV_NAME python=3.9 -y
+# Create the Conda environment from the environment.yml file
+conda env create -f environment.yml -y
 
 # Activate the environment
 echo "Activating the environment..."
 conda activate $ENV_NAME
-
-# Install pip packages from requirements.txt
-echo "Installing pip packages from requirements.txt..."
-pip install -r requirements.txt
 
 # Finished message
 echo "Environment created successfully!"
