c909127ef0df82ccd788e0b068785685028b5dde
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2025-01-14 04:53:58 +0100

    minor name fix setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 echo "Creating the Conda environment..."
 conda env create -f environment.yml
 echo "Environment created successfully!"
-echo "Activate the environment using: conda activate project_env"
+echo "Activate the environment using: conda activate XAI"
